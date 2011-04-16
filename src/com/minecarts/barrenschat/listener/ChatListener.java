958258d9a3da9658d90bc78c307c945c9e9d379c
Stephen <stephen@jazer.com>
2011-04-16 13:57:58 -0400

    Local chat messages now ignored

--- src/com/minecarts/barrenschat/listener/ChatListener.java
+++ src/com/minecarts/barrenschat/listener/ChatListener.java
@@ -13,17 +13,14 @@ import com.minecarts.barrenschat.listener.PlayerListener.RecipientData;
 import org.bukkit.event.Event;
  
  
- public class ChatListener extends CustomEventListener
- {
+ public class ChatListener extends CustomEventListener {
    private BarrensChat plugin;
  
-   public ChatListener(BarrensChat instance)
-   {
+   public ChatListener(BarrensChat instance) {
      this.plugin = instance;
    }
  
-   private static enum events
-   {
+   private static enum events {
      ChatWhisperEvent, 
      ChatChannelMessageEvent, 
      ChatChannelJoinEvent, 
@@ -36,110 +33,98 @@ import org.bukkit.event.Event;
  
      HeroicDeathEvent;
    }
-   
-   public void onCustomEvent(Event event)
-   {
+
+   public void onCustomEvent(Event event){
      try {
        events.valueOf(event.getEventName());
-     }
-     catch (IllegalArgumentException e) {
+     } catch (IllegalArgumentException e) {
        return;
      }
  
-     switch (events.valueOf(event.getEventName()))
-     {
-     case ChatWhisperEvent:
-       if (((ChatWhisperEvent)event).isCancelled()) break;
-       
-       Player sender = ((ChatWhisperEvent)event).getSender();
-       Player receiver = ((ChatWhisperEvent)event).getReceiver();
-       String msg = ((ChatWhisperEvent)event).getMessage();
- 
-       if (this.plugin.cache.ignoreList.isIgnoring(receiver, sender)) {
-         ((ChatWhisperEvent)event).setCancelled(true); //We're listening in monitor mode, and canceling, VERY VERY BAD. SHOULD NOT DO THIS.
-         sender.sendMessage(ChatColor.RED + receiver.getName() + " is ignoring you.");
-       }
-       else
-       {
-         sender.sendMessage(ChatColor.DARK_AQUA + "> [" + receiver.getName() + "] " + msg);
-         receiver.sendMessage(ChatColor.AQUA + "[" + sender.getName() + "] " + msg);
-         
-         this.plugin.whisperTracker.setWhisperSent(sender, receiver);
-         this.plugin.whisperTracker.setWhisperReceived(sender, receiver);
- 
-         this.plugin.log.info("[Whisper] " + sender.getName() + " -> " + receiver.getName() + ": " + msg);
-       }
-       break;
-     case ChatChannelMessageEvent:
-    	 
-       ChatChannelMessageEvent ccme = (ChatChannelMessageEvent)event;
-       if (ccme.isCancelled()) break;
-       
-       ccme.getChannel().chat(ccme.getPlayer(), ccme.getMessage());
-       this.plugin.log.info("[" + ccme.getChannel().name + "] " + ccme.getPlayer().getName() + ": " + ccme.getMessage());
- 
-       break;
-     case ChatChannelJoinEvent:
-       ChatChannelJoinEvent ccje = (ChatChannelJoinEvent)event;
-       if (!ccje.isCancelled()) {
-         this.plugin.channelHelper.joinChannel(ccje.getPlayer(), ccje.getChannel(), ccje.getRejoin());
-         this.plugin.log.info(String.format("[%s]: %s joined the channel", new Object[] { ccje.getChannel().name, ccje.getPlayer().getName() }));
-       }
-       break;
-     case ChatChannelLeaveEvent:
-       ChatChannelLeaveEvent ccle = (ChatChannelLeaveEvent)event;
-       if (ccle.isCancelled()) break;
-       
-       ccle.getChannel().leave(ccle.getPlayer(), !(ccle.getReason() == "QUIT")); //Alert only if it's not a quit
- 
-       if (ccle.getReason() == "COMMAND") {
-         this.plugin.dbHelper.removePlayerChannel(ccle.getPlayer(), ccle.getChannel());
-       }
- 
-       this.plugin.log.info(String.format("[%s]: %s left the channel (%s)", new Object[] { ccle.getChannel().name, ccle.getPlayer().getName(), ccle.getReason() }));
- 
-       break;
-     case ChatChannelAnnounceEvent:
-       ChatChannelAnnounceEvent ccae = (ChatChannelAnnounceEvent)event;
-       if (ccae.isCancelled()) break;
-       
-       ccae.getChannel().msg(ccae.getMessage());
- 
-       break;
-       
-     case ChatLocalMessageEvent:
-    	 ChatLocalMessageEvent clme = (ChatLocalMessageEvent)event;
-    	 if(clme.isCancelled()) break;
-    	 
-    	 for(RecipientData rd : clme.getRecipients()){
-				if(rd.distance <= 75){
-					rd.player.sendMessage(ChatColor.WHITE + clme.getMessage());
-				} else if(rd.distance <= 200){
-					rd.player.sendMessage(ChatColor.GRAY + clme.getMessage());
-				} else {
-					//They are out of range
-				}
-    	 }
-    	 break;
-     case ChatChannelDefaultChangeEvent:
-     {
-         ChatChannelDefaultChangeEvent ccdce = (ChatChannelDefaultChangeEvent)event;
+     switch (events.valueOf(event.getEventName())){
+         case ChatWhisperEvent: {
+             ChatWhisperEvent e = (ChatWhisperEvent)event;
+             if (e.isCancelled()) break;
 
-         Player player = ccdce.getPlayer();
-         ChatChannel chan = ccdce.getChannel();
+             Player sender = e.getSender();
+             Player receiver = e.getReceiver();
+             String msg = e.getMessage();
 
-         ChannelInfo defaultChannelInfo = this.plugin.dbHelper.getDefaultChannelInfo(player);
-         if ((defaultChannelInfo == null) || (defaultChannelInfo.id != chan.getId())) {
-           this.plugin.dbHelper.setDefaultChannel(player, chan);
+             sender.sendMessage(ChatColor.DARK_AQUA + "> [" + receiver.getName() + "] " + msg);
+             receiver.sendMessage(ChatColor.AQUA + "[" + sender.getName() + "] " + msg);
+
+             this.plugin.whisperTracker.setWhisperSent(sender, receiver);
+             this.plugin.whisperTracker.setWhisperReceived(sender, receiver);
+
+             this.plugin.log.info("[Whisper] " + sender.getName() + " -> " + receiver.getName() + ": " + msg);
+             break;
          }
-         break;
-     }
-     case HeroicDeathEvent:
-       HeroicDeathEvent he = (HeroicDeathEvent)event;
-       ChatChannel chan = this.plugin.channelHelper.getChannelFromName("PVP");
-       chan.msg(he.getDeathCertificate().getMessage());
-       break;
-     }
-   }
+         case ChatChannelMessageEvent: {
+             ChatChannelMessageEvent e = (ChatChannelMessageEvent)event;
+             if (e.isCancelled()) break;
 
- }
+             e.getChannel().chat(e.getPlayer(), e.getMessage());
+             this.plugin.log.info("[" + e.getChannel().name + "] " + e.getPlayer().getName() + ": " + e.getMessage());
+             break;
+         }
+         case ChatChannelJoinEvent: {
+             ChatChannelJoinEvent e = (ChatChannelJoinEvent)event;
+             if (e.isCancelled()) break;
+             
+             this.plugin.channelHelper.joinChannel(e.getPlayer(), e.getChannel(), e.getRejoin());
+             this.plugin.log.info(String.format("[%s]: %s joined the channel", new Object[] { e.getChannel().name, e.getPlayer().getName() }));
+             break;
+         }
+         case ChatChannelLeaveEvent: {
+             ChatChannelLeaveEvent e = (ChatChannelLeaveEvent)event;
+             if (e.isCancelled()) break;
+             e.getChannel().leave(e.getPlayer(), !(e.getReason() == "QUIT")); //Alert only if it's not a quit
+             if (e.getReason() == "COMMAND") {
+                 this.plugin.dbHelper.removePlayerChannel(e.getPlayer(), e.getChannel()); //They won't rejoin when they reconnect
+             }
+             this.plugin.log.info(String.format("[%s]: %s left the channel (%s)", new Object[] { e.getChannel().name, e.getPlayer().getName(), e.getReason() }));
+             break;
+         }
+         case ChatChannelAnnounceEvent: {
+             ChatChannelAnnounceEvent e = (ChatChannelAnnounceEvent)event;
+             if (e.isCancelled()) break;
+
+             e.getChannel().msg(e.getMessage());
+             break;
+         }
+         case ChatLocalMessageEvent: {
+             ChatLocalMessageEvent e = (ChatLocalMessageEvent)event;
+             if(e.isCancelled()) break;
+
+             for(RecipientData rd : e.getRecipients()){
+                 if (this.plugin.cache.ignoreList.isIgnoring(rd.player, e.getPlayer())) { continue; }
+                 if(rd.distance <= 75){
+                     rd.player.sendMessage(ChatColor.WHITE + e.getMessage());
+                 } else if(rd.distance <= 200){
+                     rd.player.sendMessage(ChatColor.GRAY + e.getMessage());
+                 } else {
+                     //They are out of range
+                 }
+             }
+             break;
+         }
+         case ChatChannelDefaultChangeEvent: {
+             ChatChannelDefaultChangeEvent e = (ChatChannelDefaultChangeEvent)event;
+             Player player = e.getPlayer();
+             ChatChannel chan = e.getChannel();
+
+             ChannelInfo defaultChannelInfo = this.plugin.dbHelper.getDefaultChannelInfo(player);
+             if ((defaultChannelInfo == null) || (defaultChannelInfo.id != chan.getId())) {
+               this.plugin.dbHelper.setDefaultChannel(player, chan);
+             }
+             break;
+         }
+         case HeroicDeathEvent: {
+           HeroicDeathEvent e = (HeroicDeathEvent)event;
+           ChatChannel chan = this.plugin.channelHelper.getChannelFromName("PVP");
+           chan.msg(e.getDeathCertificate().getMessage());
+           break;
+         }
+     }//switch
+   }//onCustomEvent
+ }//class
